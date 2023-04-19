f3150d0f39d6cc4397ba3fde160c4b8ae0d0646c
Gabriel Mullen <gabriel.mullen@snowflake.com>
2023-04-19 10:20:53 -0700

    Change target from stream to whole table

--- SQL/ckan_url_refresh_task.sql
+++ SQL/ckan_url_refresh_task.sql
@@ -1,24 +1,10 @@
 
 CREATE OR REPLACE TASK CKAN_SPOKE_URL_REFRESH
 warehouse ='OPEN_DATA_VWH'
-SCHEDULE = '1440 MINUTE' //1 DAY
+USING CRON 0 13 * * *
 AS
 BEGIN
-   update control_spoke
-    set presigned_url = purl
-FROM (
-        select get_presigned_url(@published_extracts, table_name || '.csv',604800) purl
-        from control_spoke
-    );
-
-    insert into ckan_log select localtimestamp(), 'SCHEDULED REFRESH', 'COMPLETE';
-exception
-  when other then
-    let err := object_construct('Error type', 'Other error',
-                            'SQLCODE', sqlcode,
-                            'SQLERRM', sqlerrm,
-                            'SQLSTATE', sqlstate);
-    insert into ckan_log select localtimestamp(), 'ERROR', :err::string;    
+  CALL SP_UPDATE_RESOURCES();  
 END;
 
 alter task ckan_url_refresh resume;
