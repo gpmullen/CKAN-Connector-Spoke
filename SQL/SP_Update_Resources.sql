24a03c84a3ea55ad458f51f891007158dd4a083c
Gabriel Mullen <gabriel.mullen@snowflake.com>
2023-10-16 14:00:28 -0700

    update statement moved into loop for ext

--- SQL/SP_Update_Resources.sql
+++ SQL/SP_Update_Resources.sql
@@ -16,36 +16,30 @@ BEGIN
         let rs resultset := (execute immediate 'select count(*) cnt from ' || tbl.FQTN);
         let recs cursor for rs;
         FOR rec in recs DO //the is only one record to process,but we still need to use a looping syntax
-            IF (rec.cnt > rec_limit_for_gzip) THEN //zip up the file if it's above the defined threshold
+            IF (rec.CNT > rec_limit_for_gzip) THEN //zip up the file if it's above the defined threshold
                 //drop all published files to internal stage
                 ext := '.csv.gz';
-                execute immediate ( 'copy into @published_extracts/' ||
-                IFNULL(tbl.file_name,tbl.table_name) || ext || ' from ' ||
+                execute immediate ('copy into @published_extracts/' ||
+                IFNULL(tbl.file_name,tbl.table_name) || :ext || ' from ' ||
                 tbl.FQTN || ' SINGLE = TRUE MAX_FILE_SIZE=5368709120 OVERWRITE=TRUE HEADER=TRUE file_format = (TYPE = csv COMPRESSION = GZIP NULL_IF=('''') EMPTY_FIELD_AS_NULL = FALSE FIELD_OPTIONALLY_ENCLOSED_BY=''\042'');');
             ELSE
                 //drop all published files to internal stage
-                execute immediate ( 'copy into @published_extracts/' ||
-                IFNULL(tbl.file_name,tbl.table_name) || ext || ' from ' ||
+                ext := '.csv';
+                execute immediate ('copy into @published_extracts/' ||
+                IFNULL(tbl.file_name,tbl.table_name) || :ext || ' from ' ||
                 tbl.FQTN || ' SINGLE = TRUE MAX_FILE_SIZE=5368709120 OVERWRITE=TRUE HEADER=TRUE file_format = (TYPE = csv COMPRESSION = none NULL_IF=('''') EMPTY_FIELD_AS_NULL = FALSE FIELD_OPTIONALLY_ENCLOSED_BY=''\042'');');
             END IF;
+
+            let sql string := 'UPDATE RESOURCES
+            set presigned_url = get_presigned_url(@published_extracts, IFNULL(file_name,table_name) || \'' || :ext || '\',604800)
+                , date_updated = CURRENT_TIMESTAMP()
+            WHERE resources.database_name = \'' || tbl.database_name || '\'' ||
+            ' and resources.schema_name = \'' || tbl.schema_name || '\'' ||
+            ' and resources.table_name = \'' || tbl.table_name || '\'';
+            execute immediate(:sql);
+
         END FOR;
     END FOR;
-
-//This makes another API call which stores the URL as a Resource
-                                                                    
-    update resources
-        set presigned_url = purl
-        , date_updated = CURRENT_TIMESTAMP()
-    FROM (
-            select get_presigned_url(@published_extracts, IFNULL(file_name,table_name) || ext,604800) purl
-            ,database_name
-            ,schema_name
-            ,table_name
-            from resources
-        ) r
-    where r.database_name = resources.database_name
-    and r.schema_name = resources.schema_name
-    and r.table_name = resources.table_name;
 
     insert into ckan_log 
     select current_timestamp(),null,table_name 
@@ -59,4 +53,3 @@ exception
                             'SQLSTATE', sqlstate);
     insert into ckan_log select localtimestamp(), 'ERROR', :err::string from resource_stream;
 END;
-
